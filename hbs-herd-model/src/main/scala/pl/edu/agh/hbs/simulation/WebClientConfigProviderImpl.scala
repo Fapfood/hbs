76491a7f3c3bc9e9206c784a1b508e2afd30b534
matr@grapeup.com <matr@grapeup.com>
2019-05-13 16:03:23 +0200

    add background color to simulation

--- hbs-herd-model/src/main/scala/pl/edu/agh/hbs/simulation/WebClientConfigProviderImpl.scala
+++ hbs-herd-model/src/main/scala/pl/edu/agh/hbs/simulation/WebClientConfigProviderImpl.scala
@@ -1,10 +1,11 @@
 package pl.edu.agh.hbs.simulation
 
+import pl.edu.agh.hbs.ui.dto.Colour
 import pl.edu.agh.hbs.ui.{Representation, WebClientConfigProvider}
 
-class WebClientConfigProviderImpl(val width: Int, val height: Int, val representations: Representation*) extends WebClientConfigProvider {
+class WebClientConfigProviderImpl(val width: Int, val height: Int, val colour: Colour, val representations: Representation*) extends WebClientConfigProvider {
 
-  override def getConfigString: String = "{\"width\":" + width + ", \"height\":" + height + ", \"config\":[" + representations.map(r => r.getConfig).mkString(",") + "]}"
+  override def getConfigString: String = "{\"width\":" + width + ", \"height\":" + height + ", \"color\":\"" + colour.getValue + "\", \"config\":[" + representations.map(r => r.getConfig).mkString(",") + "]}"
 
   override def getWidth: Int = width
 
