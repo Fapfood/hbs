b133b625b3e6bef238cb7594e6d9a3480eaeffc5
matr@grapeup.com <matr@grapeup.com>
2019-05-27 16:30:22 +0200

    change in env config

--- hbs-herd-model/src/main/scala/pl/edu/agh/hbs/model/EnvironmentConfig.scala
+++ hbs-herd-model/src/main/scala/pl/edu/agh/hbs/model/EnvironmentConfig.scala
@@ -1,5 +1,6 @@
 package pl.edu.agh.hbs.model
 
+import pl.edu.agh.hbs.model.skill.basic.modifier.ModPosition
 import pl.edu.agh.hbs.model.skill.patch.Patch
 
 import scala.collection.mutable
@@ -16,12 +17,23 @@ class EnvironmentConfig(val width: Int, val height: Int, val patchWidth: Int, va
     map(i) = patch
   }
 
+  def addPatches(patches: Seq[Patch]): Unit = {
+    for (patch <- patches) {
+      val position = patch.modifiers.getFirst[ModPosition].position
+      addPatch(position, patch)
+    }
+  }
+
   def getPatch(position: Vector): Patch = {
     val xMax = width / patchWidth
     val x = (position.value(0) / patchWidth).toInt
     val y = (position.value(1) / patchHeight).toInt
     val i = y * xMax + x
     map(i)
+  }
+
+  def getPatches: Seq[Patch] = {
+    map.values.toSeq
   }
 
 }
