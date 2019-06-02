ac0737887e5f6be31669c34c28be7fb137bc731c
matr@grapeup.com <matr@grapeup.com>
2019-06-02 22:31:44 +0200

    randomize velocity and add minimal value of velocity

--- hbs-herd-model/src/main/scala/pl/edu/agh/hbs/model/skill/moving/modifier/ModMoveParameters.scala
+++ hbs-herd-model/src/main/scala/pl/edu/agh/hbs/model/skill/moving/modifier/ModMoveParameters.scala
@@ -2,6 +2,6 @@ package pl.edu.agh.hbs.model.skill.moving.modifier
 
 import pl.edu.agh.hbs.model.skill.Modifier
 
-case class ModMoveParameters(velocityFactor: Double, propagationRadius: Double, maxVelocity: Double) extends Modifier {
-  override def copy(): Modifier = ModMoveParameters(velocityFactor, propagationRadius, maxVelocity)
+case class ModMoveParameters(velocityFactor: Double, propagationRadius: Double, minVelocity: Double, maxVelocity: Double) extends Modifier {
+  override def copy(): Modifier = ModMoveParameters(velocityFactor, propagationRadius, minVelocity, maxVelocity)
 }
